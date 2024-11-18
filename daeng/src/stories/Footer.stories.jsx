6e00765f2121149c5b1c0db5c06b19f731713c36
Sanghee Park <tkdgml1241@naver.com>
2024-11-19 02:14:13 +0900

    💄 Style: Footer 생성

---
+++ daeng/src/stories/Footer.stories.jsx
@@ -0,0 +1,11 @@
+import Footer from "../components/Footer";
+
+export default {
+    title: "Components/Footer",
+    component: Footer,
+    parameters: {
+        layout: "centered",
+    },
+};
+
+export const Default = {};
