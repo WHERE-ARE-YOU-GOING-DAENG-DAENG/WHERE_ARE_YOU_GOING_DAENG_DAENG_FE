5390d6d7a5fdfff8170ccee8c584313d0c1d23b7
wldusdn <wldusdngg@naver.com>
2024-11-18 17:25:50 +0900

    :lipstick: Style: 선호옵션 버튼 생성

---
+++ daeng/src/components/PreferencePlaceOption.jsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import PropTypes from 'prop-types';
+
+const CustomPreferenceButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 97px;
+  height: 73px;
+  border: 2px solid #d9d9d9;
+  border-radius: 8px;
+  background-color: white;
+  color: #000;
+  font-size: 14px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  .icon {
+    font-size: 24px;
+  }
+
+  &.selected {
+    font-weight:bold;
+    border-color: #FF4B98;
+    background-color: #FDF2F8;
+    color: #DB2877;
+  }
+`;
+
+const PreferencePlaceOption = ({ isSelected, onClick, label, icon }) => {
+  return (
+    <CustomPreferenceButton
+      className={isSelected ? "selected" : ""}
+      onClick={onClick}
+    >
+      <span className="icon">
+        <img src={icon} alt="icon"/>
+      </span>
+      <span>{label}</span>
+    </CustomPreferenceButton>
+  );
+};
+
+
+PreferencePlaceOption.propTypes = {
+    isSelected: PropTypes.bool.isRequired, 
+    onClick: PropTypes.func.isRequired,    
+    label: PropTypes.string.isRequired,    
+    icon: PropTypes.string.isRequired,
+  };
+
+export default PreferencePlaceOption;
