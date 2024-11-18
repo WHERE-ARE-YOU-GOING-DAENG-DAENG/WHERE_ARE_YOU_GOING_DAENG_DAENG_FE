bfa799bbd0a97a70e83c511e0fb811e1917315f4
Sanghee Park <tkdgml1241@naver.com>
2024-11-18 16:51:34 +0900

    💄 Style: ConfirmBtn 생성

---
+++ daeng/src/components/ConfirmBtn.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  width: 483px; 
+  height: 56px;
+  border-radius: 5px;
+  background-color: #ff69a9;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: bold;
+  border: none; 
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #FF99C8;
+  }
+
+  &:active {
+    background-color: #cc4f87;
+  }
+`;
+
+function ConfirmBtn({ label }) {
+  return <StyledButton>{label}</StyledButton>;
+}
+
+// PropTypes 정의
+ConfirmBtn.propTypes = {
+  label: PropTypes.string.isRequired, // label은 문자열이며 필수
+};
+
+export default ConfirmBtn;
