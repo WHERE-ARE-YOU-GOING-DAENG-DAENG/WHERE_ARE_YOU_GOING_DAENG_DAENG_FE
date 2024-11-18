53bf4d3dcc2e18000b814da67e00954fafdb18d3
subinsong01 <haely97@naver.com>
2024-11-18 17:35:20 +0900

    :lipstick: Style: 즐겨찾기 리스트 퍼블리싱 완료

---
+++ daeng/src/stories/FavoriteList.stories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FavoriteList from "../components/FavoriteList";
+
+export default {
+  title: "Components/FavoriteList",
+  component: FavoriteList,
+};
+
+const Template = (args) => <FavoriteList {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "가평 트리하우스",
+  place: "경기 가평군 상면 수목원로 101-16",
+  time: "영업시간 | 10:00 - 22:00",
+  color: "#FF69A9",
+};
