fa2b304eec95653150e24ab1a7602efc2b951ebe
subinsong01 <haely97@naver.com>
2024-11-19 13:41:05 +0900

    :lipstick: Style: 전역 레이아웃 설정

--- daeng/src/main.jsx
+++ daeng/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import './style/font.css'
+import GlobalStyle from './style/GlobalStyles'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
   </StrictMode>,
 )
