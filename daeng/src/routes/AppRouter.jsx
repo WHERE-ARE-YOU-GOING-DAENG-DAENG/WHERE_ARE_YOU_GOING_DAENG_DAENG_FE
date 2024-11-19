b5051063261424741c0b04ecb431347f2a633404
subinsong01 <haely97@naver.com>
2024-11-20 00:54:08 +0900

    :wrench: Chore: 반려동물 수정 페이지 라우터 설정

--- daeng/src/routes/AppRouter.jsx
+++ daeng/src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import PetRegisterPage from '../pages/pet/PetRegisterPage';
 import AddPetPage from '../pages/pet/AddPetPage';
+import PetEditPage from '../pages/pet/PetEditPage';
 
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
         <Route path="/" element={<Home />} />
         <Route path="/pet-register" element={<PetRegisterPage />} />
         <Route path="/pet-add" element={<AddPetPage />} />
+        <Route path="/pet-edit" element={<PetEditPage />} />
       </Routes>
     </Router>
   );
