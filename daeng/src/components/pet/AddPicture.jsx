bfad54f7e6cf6e3ff3bb4cf89863bf3f3ea3e712
subinsong01 <haely97@naver.com>
2024-11-20 14:22:49 +0900

    :lipstick: Style: 반려동물 CRUD 반응형 작업 완료

--- daeng/src/components/pet/AddPicture.jsx
+++ daeng/src/components/pet/AddPicture.jsx
@@ -2,13 +2,21 @@ import React from 'react'
 import styled from "styled-components";
 
 const PictureContainer = styled.button`
-  width: 135px;
-  height: 135px;
+  width: 25vw; 
+  height: 25vw; 
+  max-width: 135px; 
+  max-height: 135px; 
   border-radius: 100px;
   background-color: #FBC9E4;
   display: flex;
-  margin-right:22px;
-  border:none;
+  margin-right: 22px;
+  border: none;
+  margin-left:5%;
+
+  @media (max-width: 768px) {
+    width: 100%; 
+    height: 100vh; 
+  }
 `
 
 function AddPicture() {
--- daeng/src/pages/Home.jsx
+++ daeng/src/pages/Home.jsx
@@ -5,11 +5,28 @@ function SomeComponent() {
   const navigate = useNavigate(); 
 
   const handleClick = () => {
+    navigate('/pet-register');
+  };
+
+  const handleClickEdit = () => {
+    navigate('/pet-edit');
+  };
+
+  const handleClickAdd = () => {
+    navigate('/pet-add');
+  };
+
+  const handleClickAlarm = () => {
     navigate('/alarm');
   };
 
   return (
+    <>
     <button onClick={handleClick}>Go to Pet Register</button>
+    <button onClick={handleClickEdit}>Go to Pet Edit</button>
+    <button onClick={handleClickAdd}>Go to Pet Add</button>
+    <button onClick={handleClickAlarm}>Go to Alarm</button>
+    </>
   );
 }
 
