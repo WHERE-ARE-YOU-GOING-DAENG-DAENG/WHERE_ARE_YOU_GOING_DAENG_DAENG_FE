91eb4d799553f35498dbe7c5c5801034d42c657a
wldusdn <wldusdngg@naver.com>
2024-11-20 14:29:58 +0900

    :sparkles: Feat: 검색페이지 생성

--- daeng/src/routes/AppRouter.jsx
+++ daeng/src/routes/AppRouter.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+import Search from '../pages/Search';
 
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </Router>
   );
