864ad9f42707c0564b60aef622773eb9e43ff095
subinsong01 <haely97@naver.com>
2024-11-21 18:45:08 +0900

    :fire: Delete: 유저 이미지 아이콘 삭제

--- daeng/src/components/user/MypageFullContainer.jsx
+++ daeng/src/components/user/MypageFullContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import UserContainer from './UserContainer'
-import { styled } from 'styled-components'
+import styled from 'styled-components'
 
 const FullContainer = styled.div`
   display: block;
