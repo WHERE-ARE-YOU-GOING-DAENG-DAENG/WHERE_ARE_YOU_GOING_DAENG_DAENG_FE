18e7b58bf5a042c4b278d56c95da77a9ee4f1d82
Sanghee Park <tkdgml1241@naver.com>
2024-11-22 01:06:09 +0900

    💄 Style: 선호도 선택페이지 옵션 처리

--- daeng/src/components/review/PreferenceFavoriteOptionList.jsx
+++ daeng/src/components/review/PreferenceFavoriteOptionList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from "styled-components";
 import PreferenceFavoriteOption from '../commons/PreferenceFavoriteOption';
 import dogfood from '../../assets/icons/dogFood.svg';
