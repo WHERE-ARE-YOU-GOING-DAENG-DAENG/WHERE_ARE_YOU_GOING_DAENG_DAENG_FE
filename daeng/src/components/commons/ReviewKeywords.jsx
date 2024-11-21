704a9c4dc1cd8d08cd3cfa62b6629cd58b16f5cd
subinsong01 <haely97@naver.com>
2024-11-21 13:41:52 +0900

    :lipstick: Style: 리뷰 키워드 수정

--- daeng/src/components/commons/ReviewKeywords.jsx
+++ daeng/src/components/commons/ReviewKeywords.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
 import styled from "styled-components";
-
+import PropTypes from "prop-types";
 
 const ReviewButton = styled.button`
-  width: 95px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
   height: 24px;
   background-color:#F9A9D4;
   border-radius: 20px;
@@ -12,18 +14,31 @@ const ReviewButton = styled.button`
   font-size: 10px;
   margin-right:2%;
 
+  img{
+    margin-right: 5px;
+  }
+  &:hover {
+    background-color:#FF99C8;
+    font-weight:bold;
+  }
+
   @media (max-width: 554px) {
-    width: 90px;
     height: 20px;
     font-size: 9px;
   }
 `
-function ReviewKeywords({label}) {
+function ReviewKeywords({label, icon}) {
   return (
     <ReviewButton >
+      {icon && <img src={icon} alt="아이콘" />}
       {label}
     </ReviewButton>
   )
-} //아이콘 넣기
+}
+
+ReviewKeywords.propTypes = {
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
 
 export default ReviewKeywords;
