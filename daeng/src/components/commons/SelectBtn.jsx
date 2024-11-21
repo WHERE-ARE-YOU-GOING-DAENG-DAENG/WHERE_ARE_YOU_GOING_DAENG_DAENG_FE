7df10fb75148d5f004b2ce7c93758ace74c4f8f0
subinsong01 <haely97@naver.com>
2024-11-22 03:24:58 +0900

    :lipstick: Style: 유저 정보 등록하기 페이지 완성

--- daeng/src/components/commons/SelectBtn.jsx
+++ daeng/src/components/commons/SelectBtn.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import addressarrow from "../../assets/icons/addressarrow.svg";
 
 const StyledButton = styled.button`
     width: 100%;    
@@ -14,6 +15,7 @@ const StyledButton = styled.button`
     text-align: center;
     margin-right: 18px;
     cursor: pointer;
+    margin-bottom: 10px;
 
     ${(props) => props.selected && `
         background-color: #FF69A9;
@@ -34,14 +36,31 @@ const StyledButton = styled.button`
     }
 `;
 
-const SelectBtn = ({ label, selected, onClick }) => {
-    return <StyledButton selected={selected} onClick={onClick}>{label}</StyledButton>;
+const Icon = styled.img`
+    width: 16px;
+    height: 16px;
+    margin-top:1px;
+`;
+
+const SelectBtn = ({ label, selected, onClick, isAddress }) => {
+    return (
+        <StyledButton selected={selected} onClick={onClick}>
+            {label}
+            {isAddress && <Icon src={addressarrow} alt="주소 리스트 화살표" />}
+        </StyledButton>
+    );
 };
 
 SelectBtn.propTypes = {
     label: PropTypes.string.isRequired,
     selected: PropTypes.bool.isRequired,
     onClick: PropTypes.func.isRequired,
+    isAddress: PropTypes.bool, 
 };
+
+SelectBtn.defaultProps = {
+    isAddress: false,
+};
+
 
 export default SelectBtn;
---
+++ daeng/src/components/user/UserRegister.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import styled from 'styled-components';
+import SelectLabel from '../../components/commons/SelectLabel';
+import kakaoBtn from '../../assets/icons/kakaoBtn.svg';
+import SelectBtn from '../../components/commons/SelectBtn';
+import ConfirmBtn from '../../components/commons/ConfirmBtn';
+
+const UserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 3%;
+  margin-left: 4%;
+`;
+
+const InputEmailContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: rgba(228, 228, 228, 0.2);
+  width: 95%;
+  height: 44px;
+  border-radius: 5px;
+  border: 0.5px solid #E4E4E4;
+  color: black;
+  padding: 0 10px;
+  margin-bottom: 20px;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  align-items: center;
+  width: 95%;
+  height: 44px;
+  border-radius: 5px;
+  border: 0.5px solid #E4E4E4;
+  color: black;
+  padding: 0 10px;
+  margin-bottom:10px;
+`
+
+const Input = styled.input`
+  flex: 1;
+  border: none;
+  background: transparent;
+  outline: none;
+  font-size: 12px;
+  color: black;
+  ::placeholder {
+    color: #B3B3B3;
+  }
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+  margin-right:10px;
+`;
+
+const DuplicateBtn = styled.button`
+  width: 15%;
+  height: 23px;
+  border-radius:10px;
+  border:none;
+  font-size: 10px;
+  cursor: pointer;
+  background-color: #FF69A9;
+  color:white;
+
+  &:hover {
+    background-color: #F9A9D4;
+  }
+`
+const InputAlert = styled.p`
+  color: #FF69A9;
+  font-size: 10px;
+  display: flex;
+  margin-top: -1px;
+  flex-direction: flex-start;
+  margin-bottom: 4%;
+`;
+
+const SelectionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const AlertTitle = styled.p`
+  margin-top: 20px;
+  font-size: 14px;
+  color: black;
+  font-weight: bold;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: flex-start;
+`;
+
+function UserRegister() {
+  return (
+    <UserContainer>
+      <SelectLabel label="이메일" />
+      <InputEmailContainer>
+        <Input type="email" placeholder="이메일 받아오기" />
+        <Icon src={kakaoBtn} alt="카카오 로그인" />
+      </InputEmailContainer>
+      <SelectLabel label="닉네임" />
+      <InputBox>
+        <Input type="text" placeholder="사용하실 닉네임을 입력해 주세요." />
+        <DuplicateBtn>중복확인</DuplicateBtn>
+      </InputBox>
+      <InputAlert>*닉네임은 최소 1자 이상 작성해 주세요. 특수문자는 사용할 수 없습니다.</InputAlert>
+      <SelectLabel label="성별" />
+      <SelectionContainer>
+      <SelectBtn label="남자" />
+      <SelectBtn label="여자" />
+      </SelectionContainer>
+      <SelectLabel label="주소" />
+      <SelectionContainer>
+      <SelectBtn label="시" selected={false}  isAddress={true} />
+      <SelectBtn label="군" selected={false}  isAddress={true} />
+      </SelectionContainer>
+      <InputAlert>*보호자님과 우리 댕댕이 맞춤 장소 추천을 위해 필요한 정보입니다.</InputAlert>
+      <AlertTitle>
+        알림 푸시
+      </AlertTitle>
+      <SelectLabel label="알림 동의" />
+      <SelectionContainer>
+        <SelectBtn label="받을래요" />
+        <SelectBtn label="괜찮아요" />
+      </SelectionContainer>
+      <InputAlert>*장소에 함께하는 댕댕이를 알려드려요 </InputAlert>
+      <ConfirmBtn label='다음' />
+    </UserContainer>
+  );
+}
+
+export default UserRegister;
