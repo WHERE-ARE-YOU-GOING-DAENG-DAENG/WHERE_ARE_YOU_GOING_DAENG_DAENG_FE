3853b4d8741bd8ca2bf89ba968f648825a56dc4e
subinsong01 <haely97@naver.com>
2024-11-21 18:54:50 +0900

    :lipstick: Style: 유저 부분 완료

--- daeng/src/components/user/UserContainer.jsx
+++ daeng/src/components/user/UserContainer.jsx
@@ -1,17 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
+import arrow from '../../assets/icons/arrow.svg';
 
 const Container = styled.div`
-  
-
+  display: flex;
+  flex-direction: row;
+  margin-top:10px;
+  justify-content: space-between;
 `
 
+const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  font-weight: 500;
+
+  username{
+    font-size:15px;
+    display: block;
+  }
+
+  useremail{
+    font-size:13px;
+    color: #818181;
+    margin-top: 10px;
+  }
+`
+const ArrowImg = styled.img`
+  margin-right: 2%;
+  cursor: pointer;
+`
 function UserContainer() {
   return (
-    <>
-
-    </>
+    <Container>
+      <UserInfo>
+        <username>내가 진짜임</username>
+        <useremail>이메일</useremail>
+      </UserInfo>
+      <ArrowImg src={arrow} alt="유저 정보 자세히 보기 화살표" />
+    </Container>
   )
-}//근데 유저 이미지가 들어가는게 맞을까
+}
 
 export default UserContainer
