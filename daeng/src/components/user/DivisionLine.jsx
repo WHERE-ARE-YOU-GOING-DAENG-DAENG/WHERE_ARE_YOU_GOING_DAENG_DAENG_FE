b53c30bdefd08482676f8659b8711548caa20e7c
subinsong01 <haely97@naver.com>
2024-11-21 22:07:47 +0900

    :lipstick: Style: 마이페이지 보호자 활동 완성 & 라우터 설정

--- daeng/src/components/user/DivisionLine.jsx
+++ daeng/src/components/user/DivisionLine.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 const Division = styled.div`
   width: 100%;
   height:10px;
+  margin-top: 5px;
   background-color: rgba(217, 217, 217, 0.24);
 `
 function DivisionLine() {
