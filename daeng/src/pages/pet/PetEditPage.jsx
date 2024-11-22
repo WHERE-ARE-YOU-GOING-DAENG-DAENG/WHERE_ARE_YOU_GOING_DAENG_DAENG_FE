12b1b8caf9a34f06dbc2012a881bb8333559aacd
subinsong01 <haely97@naver.com>
2024-11-23 02:52:30 +0900

    :art: Refactor: 반려동물 등록 수정 폼 완료

--- daeng/src/pages/pet/PetEditPage.jsx
+++ daeng/src/pages/pet/PetEditPage.jsx
@@ -1,23 +1,12 @@
 import React from 'react'
-import styled from "styled-components";
 import Header from '../../components/commons/Header'
 import EditInputForm from '../../components/pet/EditInputForm';
 
-const FirstContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin-bottom: 10px;
-  margin-top: 10px;
-`
 
 function PetEditPage() {
   return (
     <>
     <Header label="댕댕이 수정" />
-    <FirstContainer>
-
-    </FirstContainer>
     <EditInputForm />
     </>
   )
