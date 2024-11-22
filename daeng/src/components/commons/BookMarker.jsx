270ff3f5b302bf2dfa6e102f9ffea0bb6c89fad7
wldusdn <wldusdngg@naver.com>
2024-11-22 12:04:43 +0900

    :lipstick: Style: 북마커 공통 컴포넌트 수정

--- daeng/src/components/commons/BookMarker.jsx
+++ daeng/src/components/commons/BookMarker.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import PropTypes from 'prop-types';
-import bookmarker from '../../assets/icons/bookmarker.svg'
 
 const CustomMarker = styled.div`
     display: flex;
@@ -13,12 +12,21 @@ const CustomMarker = styled.div`
     text-align: center;
     gap: 1px;
     word-break: keep-all;
+    
+    span {
+    color: black;
+    text-shadow: //글자 테두리
+      -1px -1px 0 white
+      1px -1px 0 white,
+      -1px 1px 0 white,
+      1px 1px 0 white;
+  }
 `
 
-const BookMarker = ({ label }) => {
+const BookMarker = ({ label, icon }) => {
     return (
         <CustomMarker>
-            <img src={bookmarker}/>
+            <img src={icon} alt="마커"/>
             <span>{label}</span>
         </CustomMarker>
     );
@@ -26,6 +34,7 @@ const BookMarker = ({ label }) => {
 
 BookMarker.propTypes = {
     label: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
 }
 
 export default BookMarker;
