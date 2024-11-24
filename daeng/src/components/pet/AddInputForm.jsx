b6e672ffeab26f437516db3676a4e650f0e67da2
subinsong01 <haely97@naver.com>
2024-11-24 23:29:36 +0900

    :lipstick: Style: 반려동물 추가등록 유효성 검사 오류 시 sweet alert로 처리

--- daeng/src/components/pet/AddInputForm.jsx
+++ daeng/src/components/pet/AddInputForm.jsx
@@ -246,29 +246,59 @@ function AddInputForm() {
     const nameRegex = /^[가-힣a-zA-Z\s]+$/;
   
     if (!petName || !nameRegex.test(petName)) {
-      alert("댕댕이 이름은 한글 또는 영문만 입력 가능합니다.");
+      AlertDialog({
+        mode: "alert", 
+        title: "입력 오류",
+        text: "댕댕이 이름은 한글 또는 영문만 입력 가능합니다.",
+        confirmText: "확인"
+      });
       return false;
     }
     if (!selectedPetType) {
-      alert("견종을 선택해주세요.");
+      AlertDialog({
+        mode: "alert", 
+        title: "선택 오류",
+        text: "댕댕이 견종을 선택해주세요",
+        confirmText: "확인"
+      })
       return false;
     }
 
     if(!selectedPetBirth) {
-      alert("생일을 선택해주세요");
+      AlertDialog({
+        mode: "alert", 
+        title: "선택 오류",
+        text: "댕댕이 생일을 선택해주세요",
+        confirmText: "확인"
+      })
       return false;
     }
 
     if (!selectedGender) {
-      alert("성별을 선택해주세요.");
+      AlertDialog({
+        mode: "alert", 
+        title: "선택 오류",
+        text: "댕댕이 성별을 선택해주세요",
+        confirmText: "확인"
+      })
       return false;
     }
     if (!selectedNeutering) {
-      alert("중성화 여부를 선택해주세요.");
+      AlertDialog({
+        mode: "alert", 
+        title: "선택 오류",
+        text: "댕댕이 중성화 여부를 선택해주세요",
+        confirmText: "확인"
+      })
       return false;
     }
     if (!selectedWeight || !petSizeOptions.some(option => option.code === selectedWeight)) {
-      alert("반려견의 크기를 선택해주세요.");
+      AlertDialog({
+        mode: "alert", 
+        title: "선택 오류",
+        text: "댕댕이 크기를 선택해주세요",
+        confirmText: "확인"
+      })
       return false;
     }
     return true;
