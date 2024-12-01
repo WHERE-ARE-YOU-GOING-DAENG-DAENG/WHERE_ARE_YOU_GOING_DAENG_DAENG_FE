ce7f7ba594cf38ce6e2ac5da3168dfe402cd1bb3
subinsong01 <haely97@naver.com>
2024-12-02 02:51:23 +0900

    :sparkles: Feat: 라우트 설정

--- daeng/src/components/detail/PlaceReviewList.jsx
+++ daeng/src/components/detail/PlaceReviewList.jsx
@@ -4,6 +4,7 @@ import TotalReviewForm from '../../components/review/TotalReviewForm';
 import writeIcon from "../../assets/icons/pen.svg";
 import ReviewKeywords from "../commons/ReviewKeywords";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Container = styled.div`
   margin: 10px 0px;
@@ -80,6 +81,7 @@ const KeywordsContainer = styled.div`
 
 const PlaceReviewList = ({ reviews }) => {
   const navigate = useNavigate();
+  const { id: placeId } = useParams();
 
   return (
     <>
@@ -87,11 +89,11 @@ const PlaceReviewList = ({ reviews }) => {
         <ReviewHeader>
           <h2>보호자님들의 리뷰</h2>
           <div className="actions">
-            <button className="action" onClick={()=> navigate("/write-review")}>
+            <button className="action" onClick={()=> navigate(`/write-review?placeId=${placeId}`)}>
               리뷰 작성
               <img src={writeIcon} alt="리뷰 작성" />
             </button>
-            <button className="action"onClick={()=> navigate("/total-review")}>전체보기 &gt;</button>
+            <button className="action"  onClick={() => navigate(`/total-review?placeId=${placeId}`)}>전체보기 &gt;</button>
           </div>
         </ReviewHeader>
 
