00892bb15980c33b2fae41ab1469ee3078cc1b66
Sanghee Park <tkdgml1241@naver.com>
2024-12-02 14:42:49 +0900

    🐛 Fix: 코드 수정

--- daeng/src/stores/useLocationStore.js
+++ daeng/src/stores/useLocationStore.js
@@ -5,10 +5,5 @@ const useLocationStore = create((set) => ({
   setUserLocation: (location) => set({ userLocation: location }),
   clearLocation: () => set({ userLocation: { latitude: 0.0, longitude: 0.0 } }),
 }));
-// const useLocationStore = create((set) => ({
-//   userLocation: { lat: 0.0, lng: 0.0 }, // 초기 위치
-//   setUserLocation: (location) => set({ userLocation: location }),
-//   clearLocation: () => set({ userLocation: { lat: 0.0, lng: 0.0 } }),
-// }));
 
 export default useLocationStore;
