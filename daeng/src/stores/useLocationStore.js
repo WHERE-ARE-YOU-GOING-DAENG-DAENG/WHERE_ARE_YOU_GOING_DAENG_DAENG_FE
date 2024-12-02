bd00d5d723d51924176224ee6160e033ac305d31
wldusdn <wldusdngg@naver.com>
2024-12-03 00:45:18 +0900

    ⏪ Rewind: 경도,위도 풀네임에서 다시 바꿈

--- daeng/src/stores/useLocationStore.js
+++ daeng/src/stores/useLocationStore.js
@@ -1,9 +1,9 @@
 import { create } from 'zustand';
 
 const useLocationStore = create((set, get, api) => ({
-  userLocation: { latitude: 0.0, longitude: 0.0 }, // 초기 위치
+  userLocation: { lat: 0.0, lng: 0.0 }, // 초기 위치
   setUserLocation: (location) => set({ userLocation: location }),
-  clearLocation: () => set({ userLocation: { latitude: 0.0, longitude: 0.0 } }),
+  clearLocation: () => set({ userLocation: { lat: 0.0, lng: 0.0 } }),
   // 구독 API를 통해 상태 변화를 감지할 수 있도록 확장
   subscribeToLocationChanges: (callback) => {
     const unsubscribe = api.subscribe(
