3d72fe8a95cdc16704a6b7fa7eb6c28c459f1525
Sanghee Park <tkdgml1241@naver.com>
2024-12-03 16:14:08 +0900

    💄 Style: 공통 컴포넌트 폰트 크기 수정

--- daeng/src/components/commons/SelectLabel.jsx
+++ daeng/src/components/commons/SelectLabel.jsx
@@ -4,12 +4,12 @@ import PropTypes from "prop-types";
 const LabelContainer = styled.p`
     display: flex;
     align-items: center;
-    font-size: 12px;
+    font-size: 14px;
     font-weight: 600;
     color: #000;
 
     span {
-        font-size: 12px;
+        font-size: 14px;
         margin-left: 4px; 
         color: #ff69a9; 
         font-weight: semibold; 
