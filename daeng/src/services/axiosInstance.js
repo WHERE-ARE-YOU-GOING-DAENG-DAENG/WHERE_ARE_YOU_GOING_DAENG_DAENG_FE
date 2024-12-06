7b2ce5edf599c377f6f2979f15732680729a2acf
wldusdn <wldusdngg@naver.com>
2024-12-06 17:44:53 +0900

    :lipstick: Style: 401에러 시 로그인 페이지로 리다이렉트

--- daeng/src/services/axiosInstance.js
+++ daeng/src/services/axiosInstance.js
@@ -9,7 +9,9 @@ export const setupAxiosInterceptors = (navigate) => {
     axiosInstance.interceptors.response.use(
         (response) => response, // 성공적인 응답 처리
         (error) => {
-            if (error.response?.status === 401 || error.response?.status === 404) {
+            if (error.response?.status === 401) {
+                navigate('/login'); // 로그인 페이지로 이동
+            }else if(error.response?.status === 404){
                 navigate('/error', { state: { status: error.response.status } }); // 에러 페이지로 이동
             }
             return Promise.reject(error); // 다른 에러는 그대로 전달
