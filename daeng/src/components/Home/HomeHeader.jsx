b1c5f9cf49bf45e6c751e01bd14bc9a9297a4ee0
subinsong01 <haely97@naver.com>
2024-12-08 00:44:17 +0900

    :bug: Fix: 알림 status 추가

--- daeng/src/components/Home/HomeHeader.jsx
+++ daeng/src/components/Home/HomeHeader.jsx
@@ -10,19 +10,9 @@ function HomeHeader() {
   const { userId } = userStore(); 
 
   const handleAlarm = () => {
-    if (userId) {
-      navigate("/alarm");
-    } else {
-      AlertDialog({
-        mode: "alert",
-        title: "로그인 필요",
-        text: "알림 페이지에 접근하려면 로그인이 필요합니다.",
-        confirmText: "확인",
-        onConfirm: () => navigate("/login"), 
-      });
-    }
+    navigate("/alarm");
   };
-
+  
   const handleLogoClick = () => {
     window.location.reload();
   };
