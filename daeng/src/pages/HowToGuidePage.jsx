e902be3af253ad17af1fe0e0c313830b8a86415e
subinsong01 <haely97@naver.com>
2024-12-08 20:49:47 +0900

    :lipstick: Style: how-to-guide 퍼블리싱 수정

--- daeng/src/pages/HowToGuidePage.jsx
+++ daeng/src/pages/HowToGuidePage.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 import Header from '../components/commons/Header';
-import howToGuide from '../assets/icons/howToGuide.svg'
+import howToGuide from '../assets/icons/howToGuide.svg';
 import FirstBanner from '../components/guide/FirstBanner';
 import SecondBanner from '../components/guide/SecondBanner';
-import Footer from "../components/commons/Footer";
+import Footer from '../components/commons/Footer';
+
+const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; 
+`;
 
 const FirstContainer = styled.div`
   width: 100%;
   height: 58px;
-  background-color: #FDF2F8;
+  background-color: #fdf2f8;
   color: black;
   font-size: 20px;
   text-align: center;
@@ -19,40 +25,43 @@ const FirstContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 10px; 
+  gap: 10px;
 
   @media (max-width: 554px) {
     font-size: 15px;
+    margin-top: 0px;
   }
 `;
 
 const Container = styled.div`
+  flex: 1;
   margin-left: 5%;
   margin-right: 5%;
 
   @media (max-width: 554px) {
-    margin-top:5%;
+    margin-bottom: 20%;
   }
-`
+`;
+
 const Icon = styled.img`
-  width: 24px; 
+  width: 24px;
   height: 24px;
 `;
 
 function HowToGuidePage() {
   return (
-    <>
+    <PageWrapper>
       <Header label="How To Guides" />
       <FirstContainer>
         댕댕어디가 서비스를 설명드려요
         <Icon src={howToGuide} alt="서비스 설명 페이지 아이콘" />
       </FirstContainer>
       <Container>
-      <FirstBanner />
-      <SecondBanner />
+        <FirstBanner />
+        <SecondBanner />
       </Container>
-      <Footer/>
-    </>
+      <Footer />
+    </PageWrapper>
   );
 }
 
