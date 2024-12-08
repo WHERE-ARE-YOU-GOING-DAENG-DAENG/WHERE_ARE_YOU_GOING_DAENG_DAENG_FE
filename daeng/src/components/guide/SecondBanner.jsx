2fa08043f18558fc1fdd6cae40046845107284c3
subinsong01 <haely97@naver.com>
2024-12-08 23:04:44 +0900

    :sparkles: Feat: banner에 화살표 추가

--- daeng/src/components/guide/SecondBanner.jsx
+++ daeng/src/components/guide/SecondBanner.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   SliderContainer,
   SlidesWrapper,
   Slide,
   IndicatorWrapper,
+  ArrowButton,
 } from "./SliderCommonStyle";
 import visit_banner1 from '../../assets/icons/banner/visit_banner1.svg'
 import visit_banner2 from '../../assets/icons/banner/visit_banner2.svg'
@@ -13,19 +14,24 @@ import visit_banner5 from '../../assets/icons/banner/visit_banner5.svg'
 import visit_banner6 from '../../assets/icons/banner/visit_banner6.svg'
 
 function SecondBanner() {
-  const banners = [visit_banner1, visit_banner2, visit_banner3, visit_banner4, visit_banner5, visit_banner6];
+  const banners = [
+    visit_banner1,
+    visit_banner2,
+    visit_banner3,
+    visit_banner4,
+    visit_banner5,
+    visit_banner6,
+  ];
   const [currentBanner, setCurrentBanner] = useState(0);
 
+  const handleNext = () => {
+    setCurrentBanner((prevBanner) => (prevBanner + 1) % banners.length);
+  };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentBanner((prevBanner) => (prevBanner + 1) % banners.length);
-    }, 5000); 
-    return () => clearInterval(interval);
-  }, [banners.length]);
-
-  const handleBannerClick = (index) => {
-    setCurrentBanner(index);
+  const handlePrev = () => {
+    setCurrentBanner((prevBanner) => 
+      (prevBanner - 1 + banners.length) % banners.length
+    );
   };
 
   return (
@@ -35,6 +41,12 @@ function SecondBanner() {
           <Slide key={index} src={banner} alt={`추천 배너 ${index + 1}`} />
         ))}
       </SlidesWrapper>
+      <ArrowButton direction="left" onClick={handlePrev}>
+        &#8592; 
+      </ArrowButton>
+      <ArrowButton direction="right" onClick={handleNext}>
+        &#8594; 
+      </ArrowButton>
       <IndicatorWrapper>
         {currentBanner + 1} / {banners.length}
       </IndicatorWrapper>
