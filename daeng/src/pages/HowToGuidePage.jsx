56e9e8c6b7a50b154518b0acea07dba2026974b1
subinsong01 <haely97@naver.com>
2024-12-08 20:41:05 +0900

    :lipstick: Style: how-to-guide 페이지 완성

---
+++ daeng/src/pages/HowToGuidePage.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from "styled-components";
+import Header from '../components/commons/Header';
+import howToGuide from '../assets/icons/howToGuide.svg'
+import FirstBanner from '../components/guide/FirstBanner';
+import SecondBanner from '../components/guide/SecondBanner';
+import Footer from "../components/commons/Footer";
+
+const FirstContainer = styled.div`
+  width: 100%;
+  height: 58px;
+  background-color: #FDF2F8;
+  color: black;
+  font-size: 20px;
+  text-align: center;
+  font-weight: bold;
+  margin-top: 10px;
+  line-height: 58px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px; 
+
+  @media (max-width: 554px) {
+    font-size: 15px;
+  }
+`;
+
+const Container = styled.div`
+  margin-left: 5%;
+  margin-right: 5%;
+
+  @media (max-width: 554px) {
+    margin-top:5%;
+  }
+`
+const Icon = styled.img`
+  width: 24px; 
+  height: 24px;
+`;
+
+function HowToGuidePage() {
+  return (
+    <>
+      <Header label="How To Guides" />
+      <FirstContainer>
+        댕댕어디가 서비스를 설명드려요
+        <Icon src={howToGuide} alt="서비스 설명 페이지 아이콘" />
+      </FirstContainer>
+      <Container>
+      <FirstBanner />
+      <SecondBanner />
+      </Container>
+      <Footer/>
+    </>
+  );
+}
+
+export default HowToGuidePage;
