29e078725b63d3f95111ff000bf8babe726a6b99
wldusdn <wldusdngg@naver.com>
2024-12-09 15:33:54 +0900

    :lipstick: Style: 땅따먹기 관련 버튼 색상들 수정

--- daeng/src/components/commons/SquareBtn.jsx
+++ daeng/src/components/commons/SquareBtn.jsx
@@ -5,9 +5,9 @@ import reviewIcon from "../../assets/icons/realtimereview.svg"
 const Button = styled.button`
   width: 100%;
   max-width: 190px;  
-  background-color: ${({ color }) => color || "#E0E0E0"};
-  color: #fff;
-  border: none;
+  background-color: ${({ mode }) => (mode === "review" ? "#FFFFFF" : "#FF69A9")};
+  color: ${({ mode }) => (mode === "review" ? "#FF69A9" : "#FFFFFF")};
+  border: ${({ mode }) => (mode === "review" ? "2px solid #FF69A9" : "none")};
   border-radius: 5px;
   padding: 9px 10px;
   display: flex;
@@ -25,7 +25,7 @@ const Button = styled.button`
   }
   @media(max-width: 554px){
     max-width: 150px;
-    font-size: 11.5px;
+    font-size: 11px;
     padding: 8px 10px;
     img{
         width: 16px;
@@ -36,17 +36,15 @@ const SquareBtn = ({mode, onClick}) => {
     const modes = {
         visit: {
             label: "방문하고 싶어요!",
-            color: "#FF69A9",
             icon: visitIcon
         },
         review:{
             label: "땅따먹기 리뷰 작성하기",
-            color: "#55D4FF",
             icon: reviewIcon
         }
     }
     return(
-        <Button color={modes[mode].color} onClick={onClick}>
+        <Button mode={mode} onClick={onClick}>
             <img src={modes[mode].icon} alt="아이콘" />
             {modes[mode].label}
         </Button>
