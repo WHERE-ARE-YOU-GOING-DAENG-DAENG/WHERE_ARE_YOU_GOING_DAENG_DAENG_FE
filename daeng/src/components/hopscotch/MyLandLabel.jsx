d0b62406dda540a99f2cd06bc4c7cbe5025178c4
wldusdn <wldusdngg@naver.com>
2024-12-11 10:11:27 +0900

    :lipstick: Style: 자기 땅 목록 컴포넌트 생성

---
+++ daeng/src/components/hopscotch/MyLandLabel.jsx
@@ -0,0 +1,43 @@
+import badge from "../../assets/icons/badge.svg"
+import styled from "styled-components";
+
+const LabelContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  background-color: #FDF2F8;
+  border-radius: 10px;
+  width: fit-content;
+`;
+
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 8px;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const MyLandLabel = ({ region, subRegion }) => {
+  return (
+    <LabelContainer>
+      <Icon>
+        <img src={badge} alt="뱃지" />
+      </Icon>
+      <TextContainer>
+        {region}<br/>{subRegion}
+      </TextContainer>
+    </LabelContainer>
+  );
+};
+
+export default MyLandLabel;
