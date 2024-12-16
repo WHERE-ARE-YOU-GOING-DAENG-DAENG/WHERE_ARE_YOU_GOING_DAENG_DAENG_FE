d2181c787fd9f1920d9e00af5dd38bdcf0d9fcd6
Sanghee Park <tkdgml1241@naver.com>
2024-12-16 17:04:17 +0900

    💄 Style: 로딩 초 변경

--- daeng/src/pages/Home.jsx
+++ daeng/src/pages/Home.jsx
@@ -92,7 +92,7 @@ function Home() {
   };
 
   const simulateLoadingDelay = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 50));
     setIsLoading(false);
   };
 
