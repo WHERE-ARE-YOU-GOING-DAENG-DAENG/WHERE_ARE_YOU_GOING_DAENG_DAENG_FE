b67f84d775f5ade0f2252d4e9e46e05e645aeca3
subinsong01 <haely97@naver.com>
2024-12-17 14:39:24 +0900

    :bug: Fix: 피드백 받은 부분 수정

--- daeng/src/stores/UseReviewStore.js
+++ daeng/src/stores/UseReviewStore.js
@@ -7,7 +7,7 @@ const useReviewStore = create((set, get) => ({
   page: 0,
   size: 15,
   isFirst: true,
-  isLast: false, 
+  isLast: false,
   isLoading: false,
   placeName: "",
   error: null,
@@ -32,7 +32,7 @@ const useReviewStore = create((set, get) => ({
         size: data.size,
         isFirst: data.isFirst,
         placeName: data.placeName || "",
-        isLast: data.isLast, 
+        isLast: data.isLast,
         isLoading: false,
       });
     } catch (error) {
@@ -40,6 +40,26 @@ const useReviewStore = create((set, get) => ({
         error.response?.data?.message || "리뷰 데이터를 가져오는 데 실패했습니다.";
       set({ isLoading: false, error: errorMessage });
       console.error("리뷰 데이터 요청 실패:", errorMessage);
+    }
+  },
+
+  removeReview: async (reviewId) => {
+    try {
+      const response = await axios.delete(
+        `https://dev.daengdaeng-where.link/api/v1/review/${reviewId}`,
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+
+      if (response.status === 200) {
+        const newReviews = get().reviews.filter((review) => review.reviewId !== reviewId);
+        set({ reviews: newReviews });
+      }
+    } catch (error) {
+      console.error("리뷰 삭제 실패:", error);
+      throw new Error("리뷰 삭제 중 오류가 발생했습니다.");
     }
   },
 
