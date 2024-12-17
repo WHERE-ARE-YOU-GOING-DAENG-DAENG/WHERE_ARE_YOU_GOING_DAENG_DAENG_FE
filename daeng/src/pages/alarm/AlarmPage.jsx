e01878f50cb6a880d1d0877c75f7b5d096b8ae60
subinsong01 <haely97@naver.com>
2024-12-17 17:35:13 +0900

    :lipstick: Style: 알림 리스트 퍼블리싱 수정

--- daeng/src/pages/alarm/AlarmPage.jsx
+++ daeng/src/pages/alarm/AlarmPage.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 import Header from '../../components/commons/Header';
 import AlarmBox from '../../components/alarm/AlarmBox';
 import Footer from '../../components/commons/Footer';
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  overflow-y: auto; 
+`;
 
 function AlarmPage() {
   return (
-    <>
+    <PageContainer>
       <Header label="알림" />
-      <AlarmBox />
+      <Content>
+        <AlarmBox />
+      </Content>
       <Footer />
-    </>
+    </PageContainer>
   );
 }
 
