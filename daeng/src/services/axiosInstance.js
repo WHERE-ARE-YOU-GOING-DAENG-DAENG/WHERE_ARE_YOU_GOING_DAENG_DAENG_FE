81f09b55ad76a7a3dcbf18e522a17539c2b728a4
wldusdn <wldusdngg@naver.com>
2025-01-24 19:47:25 +0900

    :bug: Fix: https 자동변환 없애기

--- daeng/src/services/axiosInstance.js
+++ daeng/src/services/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
     baseURL: 'http://52.79.50.171:8080',
     withCredentials: true,
-    httpsAgent: false
+    validateStatus: () => true
 });
 
 export const setupAxiosInterceptors = (navigate) => {
