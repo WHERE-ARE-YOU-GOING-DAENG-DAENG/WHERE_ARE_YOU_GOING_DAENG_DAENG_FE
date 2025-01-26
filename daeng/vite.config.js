657b0b591d863004e1b5973beebfca5276124230
wldusdn <wldusdngg@naver.com>
2025-01-27 01:28:41 +0900

    :sparkles: Feat: vite용 이미지 압축설정

--- daeng/vite.config.js
+++ daeng/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import imagemin from 'vite-plugin-imagemin';
 /*import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';*/
@@ -17,5 +18,34 @@ export default defineConfig({
   cert: fs.readFileSync(path.resolve(__dirname, 'localhost+2.pem')),
   },
   },*/
-  plugins: [react()],
+  plugins: [
+    react(),
+    imagemin({
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 7 },
+      mozjpeg: { quality: 75 },
+      webp: {
+        quality: 75,
+        lossless: false, // true: 무손실 압축 (파일 크기 클 수 있음)
+      },
+      pngquant: {
+        quality: [0.7, 0.9], // PNG 압축 품질 범위 설정
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          { removeViewBox: false },
+          { removeEmptyAttrs: false },
+        ],
+      },
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0, // base64 변환 방지 (모든 이미지를 파일로 유지)
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]", // 빌드 시 이미지 파일명 고유하게 유지
+      },
+    },
+  },
 })
