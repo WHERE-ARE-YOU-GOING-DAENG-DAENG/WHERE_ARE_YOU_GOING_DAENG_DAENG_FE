52cee4b8666fdd46d38f02ba594135712a8b219f
wldusdn <wldusdngg@naver.com>
2025-02-06 22:27:30 +0900

    :art: Refactor: 서드파티쿠키 주석처리

--- daeng/src/main.jsx
+++ daeng/src/main.jsx
@@ -4,59 +4,59 @@ import App from "./App.jsx";
 import "./style/font.css";
 import GlobalStyle from "./style/GlobalStyles";
 
-const naverSiteId = import.meta.env.VITE_NAVER_SITE_ID;
+// const naverSiteId = import.meta.env.VITE_NAVER_SITE_ID;
 
-function addNaverAnalyticsScript() {
-  if (naverSiteId) {
-    if (!window.wcs) {
-      const naverScript = document.createElement("script");
-      naverScript.src = "//wcs.naver.net/wcslog.js";
-      naverScript.onload = () => {
-        if (!window.wcs_add) window.wcs_add = {};
-        window.wcs_add["wa"] = naverSiteId;
-        window.wcs_do();
-      };
-      document.head.appendChild(naverScript);
-    }
-  } else {
-    console.warn("Naver Analytics Site ID가 설정되지 않았습니다.");
-  }
-}
+// function addNaverAnalyticsScript() {
+//   if (naverSiteId) {
+//     if (!window.wcs) {
+//       const naverScript = document.createElement("script");
+//       naverScript.src = "//wcs.naver.net/wcslog.js";
+//       naverScript.onload = () => {
+//         if (!window.wcs_add) window.wcs_add = {};
+//         window.wcs_add["wa"] = naverSiteId;
+//         window.wcs_do();
+//       };
+//       document.head.appendChild(naverScript);
+//     }
+//   } else {
+//     console.warn("Naver Analytics Site ID가 설정되지 않았습니다.");
+//   }
+// }
 
-function addGoogleAdsScript() {
-  const gtagId = import.meta.env.VITE_GTAG_ID;
+// function addGoogleAdsScript() {
+//   const gtagId = import.meta.env.VITE_GTAG_ID;
 
-  if (!gtagId) {
-    console.warn("Google Ads ID가 설정되지 않았습니다.");
-    return;
-  }
+//   if (!gtagId) {
+//     console.warn("Google Ads ID가 설정되지 않았습니다.");
+//     return;
+//   }
 
-  const placeholder = document.getElementById("google-ads-placeholder");
-  if (!placeholder) {
-    console.warn("Google Ads placeholder가 HTML에서 누락되었습니다.");
-    return;
-  }
+//   const placeholder = document.getElementById("google-ads-placeholder");
+//   if (!placeholder) {
+//     console.warn("Google Ads placeholder가 HTML에서 누락되었습니다.");
+//     return;
+//   }
 
-  const script1 = document.createElement("script");
-  script1.async = true;
-  script1.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
-  placeholder.parentNode.insertBefore(script1, placeholder);
+//   const script1 = document.createElement("script");
+//   script1.async = true;
+//   script1.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
+//   placeholder.parentNode.insertBefore(script1, placeholder);
 
 
-  const script2 = document.createElement("script");
-  script2.innerHTML = `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${gtagId}');
-  `;
-  placeholder.parentNode.insertBefore(script2, placeholder);
+//   const script2 = document.createElement("script");
+//   script2.innerHTML = `
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag(){dataLayer.push(arguments);}
+//     gtag('js', new Date());
+//     gtag('config', '${gtagId}');
+//   `;
+//   placeholder.parentNode.insertBefore(script2, placeholder);
 
-  placeholder.remove();
-}
+//   placeholder.remove();
+// }
 
-addNaverAnalyticsScript();
-addGoogleAdsScript();
+// addNaverAnalyticsScript();
+// addGoogleAdsScript();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
